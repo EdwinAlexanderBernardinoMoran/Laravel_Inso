c1bcad439a974c1fb7ea013931b8c4a5a5c608a9
Edwin Alexander Bernardino Moran <814892@clases.edu.sv>
2022-10-06 15:53:59 -0600

    Creando Controlador y Modelo de Discapacidad

---
+++ app/Http/Controllers/DiscapacidadesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Discapacidades;
+use Illuminate\Http\Request;
+
+class DiscapacidadesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Discapacidades  $discapacidades
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Discapacidades $discapacidades)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Discapacidades  $discapacidades
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Discapacidades $discapacidades)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Discapacidades  $discapacidades
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Discapacidades $discapacidades)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Discapacidades  $discapacidades
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Discapacidades $discapacidades)
+    {
+        //
+    }
+}
