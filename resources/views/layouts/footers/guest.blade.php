6d3bcd2d0fa47af509d6fd268e44bfb7bee13fbe
Edwin Alexander Bernardino Moran <814892@clases.edu.sv>
2022-11-09 15:20:20 -0600

    Terminando la parte del Login de la App de Inso Asistencia

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <div class="copyright center">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, Instituto Nacional De Sonzacate made with <i class="material-icons">favorite</i> by
+            <a href="https://www.facebook.com/edwinalexander.bernardinomoran.5" target="_blank">Edwin Alexander Bernardino Morán</a>
+            </div>
+    </div>
+</footer>
